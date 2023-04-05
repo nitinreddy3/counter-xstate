861e746c5a44ea5c3ef0376b48329efb7492324c
Nitin Reddy <nitin.reddy@iglooinsure.com>
2023-04-05 10:09:48 +0530

    Updated the state machine

--- utils/counterMachine.ts
+++ utils/counterMachine.ts
@@ -21,20 +21,20 @@ export const counterMachine = createMachine({
   },
 });
 
-export const todoMachine = createMachine({
-  context: {
-    // Here, we will define the initial state of the machine
-    todos: []
-  },
-  on: {
-    // Here we will define the events that will trigger the transitions.
-    ADD_TODO: {
-      actions: assign({
-        todos: (context, event) => [
-          ...context.todos,
-          event.todo,
-        ]
-      })
-    }
-  },
-});
+// export const todoMachine = createMachine({
+//   context: {
+//     // Here, we will define the initial state of the machine
+//     todos: []
+//   },
+//   on: {
+//     // Here we will define the events that will trigger the transitions.
+//     ADD_TODO: {
+//       actions: assign({
+//         todos: (context, event) => [
+//           ...context.todos,
+//           event.todo,
+//         ]
+//       })
+//     }
+//   },
+// });
